7f6c942902e93fdacb5ed7b2d0873f4d7f5705ca
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-08 22:26:21 +0200

    feat: add products by category page

---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,17 @@
+import ProductsList from "@/app/components/productsList/ProductsList";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Category Name"
+};
+
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = await params;
+    const { products } = await fetch(`https://dummyjson.com/products/category/${slug}`).then(res => res.json());
+
+    return (
+        <main className="p-4">
+            <ProductsList products={products} />
+        </main>
+    );
+}
