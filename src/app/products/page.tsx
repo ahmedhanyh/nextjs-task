bf0f1afeffecf66542ece8b8e302cd850c25f699
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-08 21:05:05 +0200

    feat: show each product image and price
    - organize the products in a grid

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -8,18 +8,28 @@ export const metadata: Metadata = {
 export default async function Products() {
   const response = await fetch("https://dummyjson.com/products");
   const { products } = await response.json();
+  console.log(products[0]);
 
   return (
-    <ul>
-      {
-        products.map(product => {
-          return (
-            <Link href={`/products/${product.id}`} key={product.id}>
-              <li>{product.title}</li>
-            </Link>
-          );
-        })
-      }
-    </ul>
+    <main className="p-4">
+      <ul className="grid grid-cols-4 gap-4">
+        {
+          products.map(({ id, title, thumbnail, price }) => {
+            return (
+              <Link href={`/products/${id}`} key={id}>
+                <li className="p-2 h-full shadow-sm rounded-lg flex flex-col justify-between transition-all duration-200 hover:scale-104 hover:shadow-md">
+                  {/* <img src={product.images[0]} alt="" className="h-3/4 self-center" /> */}
+                  <img src={thumbnail} alt="" className="h-3/4 self-center" />
+                  <div>
+                    <h2>{title}</h2>
+                    <p>${price}</p>
+                  </div>
+                </li>
+              </Link>
+            );
+          })
+        }
+      </ul>
+    </main>
   );
 }
