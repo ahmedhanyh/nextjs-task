911af23f9c8a0b616c1ab6a22111a7bae5017b92
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-08 21:13:47 +0200

    style: change Navbar styling

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -9,12 +9,12 @@ export default function Navbar() {
 
     return (
         <nav>
-            <ul className="navlist list-none">
+            <ul className="navlist list-none border-b border-gray-200 flex justify-center">
                 {
                     navItems.map(({ title, path }) => {
                         return (
                             <Link href={path} key={path}>
-                                <li className="nav-item">{title}</li>
+                                <li className="nav-item p-4 font-bold hover:text-white hover:bg-slate-950">{title}</li>
                             </Link>
                         );
                     })
