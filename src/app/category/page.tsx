28e489a67da776aa399c1b3c4da0de748d353753
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-08 23:07:03 +0200

    style: update category style

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -11,12 +11,14 @@ export default async function Category() {
 
   return (
     <main className="p-4">
-      <ul className="flex flex-wrap gap-2">
+      <ul className="flex flex-col gap-y-2">
         {
           categories.map(category => {
             return (
-              <Link href={`/category/${category.slug}`} key={category.slug}>
-                <li className="w-max px-4 py-2 border rounded-sm text-center hover:bg-slate-950 hover:text-white">{category.name}</li>
+              <Link href={`/category/${category.slug}`} key={category.slug}
+                    className="py-4 border-b border-gray-300 transition-all duration-200 hover:scale-101 hover:border-gray-600">
+                <li className="ps-8 text-xl font-bold">{category.name}</li>
+                {/* <li className="w-max px-4 py-2 border rounded-sm text-center hover:bg-slate-950 hover:text-white">{category.name}</li> */}
               </Link>
             );
           })
