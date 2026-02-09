84d0c97980fd902c4a360b4ea869f3b4a8ae646a
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-09 15:16:56 +0200

    feat: add button links on homepage
    - style the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+"use client"
+
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="grow flex justify-center items-center">Home</main>
+    <main className="bg-linear-135 from-cyan-500 to-blue-500 grow flex flex-col justify-center items-center gap-16">
+      <h1 className="text-4xl font-black">Welcome to our Website!</h1>
+      <div className="text-lg font-bold flex flex-col justify-center items-center gap-4">
+        <Link href="/products" className="px-4 py-2 border border-slate-950 rounded-sm hover:bg-slate-950 hover:text-white">View all our Products</Link>
+        <Link href="/category" className="px-4 py-2 border border-slate-950 rounded-sm hover:bg-slate-950 hover:text-white">View Categories</Link>
+      </div>
+    </main>
   );
 }
