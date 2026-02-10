87e120bec9da0531f3727204f0714c6070466dde
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-10 20:58:40 +0200

    feat: add not-found component
    - change loading component file from .js to .tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <main className="grow flex justify-center items-center">
+            <div className="size-16 border-4 border-slate-950 border-t-transparent rounded-full animate-spin"></div>
+        </main>
+    );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <main className="grow flex justify-center items-center">
+            <h1>Couldn't find the page you're looking for</h1>
+        </main>
+    );
+}
