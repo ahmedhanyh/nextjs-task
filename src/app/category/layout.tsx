cde82fac32887f32a158e668e0f6afe7ab9cebfd
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-10 21:15:31 +0200

    feat: load only layout children
    - wrap layout children with <Suspense />

--- src/app/category/layout.tsx
+++ src/app/category/layout.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
+
 export default function CategoryLayout({
     children,
 }: { children: React.ReactNode }) {
     return (
         <div className="flex flex-col gap-4">
             <h1 className="mt-8 text-2xl font-bold text-center">Categories</h1>
-            {children}
+            <Suspense fallback={<p className="text-center font-bold">Loading categories...</p>}>
+                {children}
+            </Suspense>
         </div>
     )
 }
