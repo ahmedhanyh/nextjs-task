0cf1e35e501f9056712f2934496bb9745bb11d53
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-10 22:36:52 +0200

    style: update not-found text style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
     return (
         <main className="grow flex justify-center items-center">
-            <h1>Couldn't find the page you're looking for</h1>
+            <h1 className="text-3xl font-bold">Couldn't find the page you're looking for!</h1>
         </main>
     );
 }
