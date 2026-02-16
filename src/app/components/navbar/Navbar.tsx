8615e9830a064bbb72982c442a858b0ec0366511
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-16 23:20:46 +0200

    feat: add Login/Logout nav links
    - show authenticated user's profile picture on navbar

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,12 +1,27 @@
 import NavLink from "@/app/components/NavLink";
+import { auth } from "@/services/auth";
+import Image from "next/image";
 
-export default function Navbar() {
+export default async function Navbar() {
     const navItems = [
         { title: "Home", path: "/" },
         { title: "Products", path: "/products" },
         { title: "Category", path: "/category" },
         { title: "Cart", path: "/cart" },
     ];
+
+    const session = await auth();
+    console.log(session);
+
+    if (session) {
+        navItems.push(
+            { title: "Logout", path: "/api/auth/signout" },
+        );
+    } else {
+        navItems.push(
+            { title: "Login", path: "/api/auth/signin" },
+        );
+    }
 
     return (
         <nav>
@@ -17,6 +32,18 @@ export default function Navbar() {
                             <NavLink title={title} path={path} key={path} />
                         );
                     })
+                }
+                {
+                    session &&
+                    (<li>
+                        <Image
+                            src={session?.user?.image || ""}
+                            alt="User Avatar"
+                            width={50}
+                            height={50}
+                            className="ml-4 rounded-full"
+                        />
+                    </li>)
                 }
             </ul>
         </nav>
