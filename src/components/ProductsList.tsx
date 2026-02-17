c8e79abe2f87ed2faa702e7da2f758ed095d4c0c
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-17 08:02:11 +0200

    feat: add product description and new buttons
    - add show more/less button to expand/shrink product description.
    - add view details button to navigate to the product details page
      instead of making the entire product card a link.
    - make some style modifications.

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import AddToCart from "@/components/AddToCartButton";
+import TextExpander from "./TextExpander";
 
 export default function ProductsList({ products, rating }) {
     let filteredProducts = products;
@@ -27,17 +28,19 @@ export default function ProductsList({ products, rating }) {
                 filteredProducts.length ?
                     <ul className="grid grid-cols-4 gap-4">
                         {
-                            filteredProducts.map(({ id, title, thumbnail, price }) => {
+                            filteredProducts.map(({ id, title, description, thumbnail, price }) => {
                                 return (
-                                    <li key={id} className="p-2 h-full shadow-sm rounded-lg flex flex-col justify-between transition-all duration-200 hover:scale-104 hover:shadow-md">
-                                        <Link href={`/products/${id}`} className="flex flex-col justify-between">
-                                            <img src={thumbnail} alt="" className="h-3/4 self-center" />
-                                            <div>
-                                                <h2>{title}</h2>
-                                                <p>${price}</p>
-                                            </div>
-                                        </Link>
+                                    <li key={id} className="p-2 h-full shadow-sm rounded-lg flex flex-col justify-between gap-2 transition-all duration-200 hover:scale-104 hover:shadow-md">
+                                        <img src={thumbnail} alt="" className="h-3/4 self-center" />
+                                        <div>
+                                            <h2 className="font-extrabold">{title}</h2>
+                                            <div className="mt-2 mb-8"><TextExpander>{description}</TextExpander></div>
+                                            <p className="font-bold">${price}</p>
+                                        </div>
                                         <AddToCart id={id} />
+                                        <Link href={`/products/${id}`} className="text-center py-2 border rounded-sm hover:bg-slate-950 hover:text-white">
+                                            View Details
+                                        </Link>
                                     </li>
                                 );
                             })
